3a39957e6f51d984dd12527d8d96bb0277b39502
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-09-11 20:14:39 +0530

    Initial commit
    build.sh, shell script to --/build/-- whole "iamprabhat.github.io®" in one go.
    Usage: $ sh build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+# =========================== I'm Prabhat® ===========================
+#               Technical Architect & Data Scientist
+# Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
